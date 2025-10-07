7cd484979f33596657efb9ad0be5236e2a80892c
Sichan Yoo <chanyoo@amazon.com>
2025-10-07 15:58:26 -0700

    If this doesn't work, change approach to batch memory mgmt.

--- Tests/ConcurrentIntegrationTests/ConcurrentDownloadObjectIntegTests.swift
+++ Tests/ConcurrentIntegrationTests/ConcurrentDownloadObjectIntegTests.swift
@@ -39,7 +39,7 @@ class ConcurrentDownloadObjectIntegTests: XCTestCase {
     override func setUp() async throws {
         let tmConfig = try await S3TransferManagerConfig(
             s3ClientConfig: S3Client.S3ClientConfiguration(region: region),
-            maxInMemoryBytes: 500 * 1024 * 1024
+            maxInMemoryBytes: 100 * 1024 * 1024
         )
         tm = S3TransferManager(config: tmConfig)
 
